ae3d21ac14bcce624b1f2769c87c2e20b56c8b29
Santi_2 <morenosantiago95@gmail.com>
2015-06-16 13:44:10 +0200

    test

--- app/src/main/java/com/example/daniel/myapplication/Neu.java
+++ app/src/main/java/com/example/daniel/myapplication/Neu.java
@@ -30,7 +30,7 @@ public class Neu extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this  adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_neu, menu);
         return true;
     }
